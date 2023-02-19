d36344d1c161135283019de315a8ba33b04046f2
kofi8 <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>
2023-02-19 17:09:44 +0600

    ddfsa

--- hello.rb
+++ hello.rb
@@ -3,7 +3,18 @@
 # print "Hi! Kofil"
 # print "is cool"
 
-puts "  /|"
-puts " / |"
-puts "/__|"
+# puts "  /|"
+# puts " / |"
+# puts "/__|"
+
+character_nane = "Kofil"
+character_age = "35";
+puts ("Hi! this is " + character_nane + " with aged of " + character_age);
+
+//Data TYPES
+
+name = "Koifl"
+occupation = "programmer"
+age = 22
+gpa = 2.34
 
