08c6f63d0ff7f4dfa0292021fce3b5f01b942ba5
kofi8 <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>
2023-02-19 17:26:16 +0600

    Hello

--- hello.rb
+++ hello.rb
@@ -11,10 +11,26 @@ character_nane = "Kofil"
 character_age = "35";
 puts ("Hi! this is " + character_nane + " with aged of " + character_age);
 
-//Data TYPES
+#Data TYPES
 
 name = "Koifl"
 occupation = "programmer"
 age = 22
 gpa = 2.34
+isMale = false
+isTall = true
+flaws = nil
 
+
+puts "Kofil hi what's up! 'fallout' \n hi gislalf"
+
+phrase = "Hi this is Kofil "
+puts phrase.upcase()
+puts phrase.downcase()
+puts phrase.strip()
+puts phrase.include? "Koifl"
+puts phrase[0]
+puts phrase.index("K")
+
+
+puts "Hello there is this KOifl".upcase()
